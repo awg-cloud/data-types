0d3b84763e7dce5b9ad2576a7ae54c262b75a1b2
awg-cloud <155889825+awg-cloud@users.noreply.github.com>
2024-01-07 13:29:54 +0100

    Add files via upload

---
+++ data-types.py
@@ -0,0 +1,27 @@
+# string data type
+
+# literal assignment
+first = "Dave"
+last = "Gray"
+
+# print(type(first))
+# print(type(first) == str)
+# print(isinstance(first, str))
+
+# constructor function
+# pizza = str("Pepperoni")
+# print(type(pizza))
+# print(type(pizza) == str)
+# print(isinstance(pizza, str))
+
+# Concatenation
+fullname = first + " " + last
+print(fullname)
+
+fullname += "!"
+print(fullname)
+
+# Casting a number to a string
+decade = str(1980)
+print(type(decade))
+print(decade)
